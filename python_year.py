4f4d974cd3c5cfd9656bc17c846857bc3a46fd1b
Louise Archibald <louarchibald@Louises-MacBook-Pro.local>
2022-05-03 11:50:40 +0100

    day 2 stuff

---
+++ python_year.py
@@ -0,0 +1,17 @@
+# SET guess = INPUT "What year was Python created?"
+
+guess = input("What year was Python created? ")
+
+# IF guess is "1991"
+
+if guess == "1991":
+    print("You guessed correctly!")
+
+# THEN PRINT "You guessed correctly!"
+# ELSE 
+
+else:
+    print("Incorrect! Guess again!")
+
+# PRINT "Incorrect! Guess again!"
+#END
---
+++ what_animal.py
@@ -0,0 +1,23 @@
+# SET animal = INPUT "What animal are you?"
+
+#animal = input("What animal are you?")
+
+# IF animal is "chicken" 
+
+#if animal == "chicken":
+    #print("This is my favourite animal!")
+
+# THEN PRINT "This is my favourite animal!"
+# ELSE
+# PRINT "This is not my favourite animal!"
+
+#else:
+    #print("This is not my favourite animal!")
+
+# END
+
+score = 6
+
+result = "pass" if score > 5 else "fail"
+
+print(result)
